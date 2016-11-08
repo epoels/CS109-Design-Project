5e8d39576228885b0709782850ece3866f225747
yshtalen <yshtalen@ucsc.edu>
2016-11-07 20:21:12 -0800

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -21,51 +21,113 @@ public:
 class Math_Funct : Base_Funct{
 protected:
         vector<string> params;
-        //Var* result;
+        //VAR* result;
+        vector<float> converted_params;
 
 public:
-        Math_Funct(){cout << "Math_Funct made" << endl;}
+        Math_Funct(){cout << "Math made" << endl;}
         ~Math_Funct(){cout << "Math_Funct destroyed" << endl;}
-        bool validator(){}
-                //Var* result = var_convert(params[0]);
-                //if(result!=null_ptr){continue;}
-        void process(){}
+        //check parameter number and types
+        virtual bool validator(){}
+        void process(){cout << "MATH processing..." << endl;}
 };
 class Add : Math_Funct{
 protected:
         vector<string> params;
-        //Var* result;
+        //VAR* result;
+        vector<float> converted_params;
+public:
+        Add(vector<string> input) : params(input.begin()+1, input.end()){cout << "Add made" << endl;}
+        ~Add(){cout << "Add destroyed" << endl;}
+        //check that parameter number and types are valid
+        bool validator(){
+                cout << "running ADD validator..." << endl;
+                //parameter counter
+                int num_params=0;
+                //pull variable from map
+                //result = createdVariables[input[0]];
+                //if(result==NULL){return false;}
+                //convert parameter list
+                for(string s : params){
+                        cout << s << endl;
+                        try{
+                                //if parameter is variable, pull from map
+                                if(s[0]=='$'){num_params++; /*converted_params.pushback(createdVariables[s]->getValue())*/}
+                                //if parameter is number, convert to float
+                                else if(isdigit(s[0])){num_params++; converted_params.push_back(stof(s));}
+                                //all other parameter types are invalid
+                                else{return false;}
+                        }
+                        catch(...){return false;}
+
+                        if(num_params>12){return false;}
+                }
+                return true;
+        }
+
+        //if validator is true, run add process
+        void process(){
+                if(validator()){
+                        cout << "ADD processing..." << endl;
+                        //sum values
+                        float sum = 0;
+                        for(float f : converted_params){sum+=f;}
+                        cout << sum << endl;
+                        //result->setValue(sum);
+                }
+                else{cout << "invalid parameter list" << endl;}
+
+        }
+};
+class Mult : Math_Funct{
+protected:
+        vector<string> params;
+        //VAR* result;
         vector<float> converted_params;
 
 public:
-        Add(vector<string> input) : params(input.begin()+1, input.end()-1){cout << "Add made" << endl;}
-        ~Add(){cout << "Add destroyed" << endl;}
+        Mult(vector<string> input) : params(input.begin()+1, input.end()){cout << "Mult made" << endl;}
+        ~Mult(){cout << "Mult destroyed" << endl;}
+        //check that parameter number and types are valid
         bool validator(){
+                cout << "running MULT validator..." << endl;
                 //parameter counter
                 int num_params=0;
                 //check that first element of input is NUMERIC or REAL variable
-                //Var* result = var_convert(input[0]);
+                //result = var_convert(input[0]);
 
-                //convert paramter list
-                for(string s : params){ 
+                //convert parameter list
+                for(string s : params){
+                        cout << s << endl;
                         try{
                                 //if parameter is variable, pull from map
                                 if(s[0]=='$'){num_params++; /*fetch matching var*->result();*/}
                                 //if parameter is number, convert to float
                                 else if(isdigit(s[0])){num_params++; converted_params.push_back(stof(s));}
                                 //all other paramter types are invalid
-                                else{return false;cout << "validator failed" << endl;}
+                                else{return false;}
                         }
-                        catch(...){return false;cout << "validator failed" << endl; }
+                        catch(...){return false;}
+
                         if(num_params>12){return false;}
                 }
+                return true;
+        }
+        //if validator is true, run add process
+        void process(){
+                if(validator()){
+                        cout << "MULT processing..." << endl;
+                        //sum values
+                        float prod  = 1;
+                        for(float f : converted_params){prod*=f;}
+                        cout << prod << endl;
+                        //result->setValue()=sum;
+                }
+                else{cout << "invalid paramater list" << endl;}
 
         }
-        void process(){
-                if(validator())
-                cout << "processing..." << endl;
-        }
 };
+
 
 int main(){
         vector<string> stuff;
