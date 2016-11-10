f8b63e3755a28b7f43fa69609cc44cceb55e8d5e
chris zhang <chris34481203@gmail.com>
2016-11-09 22:44:59 -0800

    Add files via upload

--- Helper Function for label and jmp.cpp
+++ Helper Function for label and jmp.cpp
@@ -23,20 +23,20 @@ void  JMPZHelper(stringstream&ss) {
 				exit(1);
 			}
 		}
-		else if (y[0] = '$') {
-			if (q != createdVariable.end() && q->second->value == 0 && p != createdLabels.end())
-				counter = p->second->value;
-			else if (q != createdVariable.end() && q->second->value != 0 && p != createdLabels.end())
-				counter = linenumber;
-			else {
-				cout << "label or variable not found\n";
-				exit(1);
-			}
-		}
+	}
+	else if (y[0] = '$') {
+		if (q != createdVariable.end() && q->second->value == 0 && p != createdLabels.end())
+			counter = p->second->value;
+		else if (q != createdVariable.end() && q->second->value != 0 && p != createdLabels.end())
+			counter = linenumber;
 		else {
-			cout << "syntax error\n";
+			cout << "label or variable not found\n";
 			exit(1);
 		}
+	}
+	else {
+		cout << "syntax error\n";
+		exit(1);
 	}
 }
 
@@ -47,35 +47,30 @@ void  JMPNZHelper(stringstream&ss) {
 	if (A != NULL) {
 		A->initialize(ss, counter);
 	}
-
-	string x = " ";
-	getline(ss, x, ' ');
 	string y = " ";
 	getline(ss, y, ' ');
-
-	map<string, int>::iterator q;
-	map<string, int>::iterator p;
-	p = labelclass.find(x);
-	if (isdigit(y[0])) {
-		float ok = stof(y);
+	map<string, *VAR>::iterator p;
+	map<string, *label>::iterator q;
+	p = createdVariable.find(y);
+	q = createdLabels.find(A->labelname);
+	if (isdigit(y)) {
+		int ok = stoi(y);
 		if (ok != 0) {
-			if (p != labelclass.end())
-				counter = p->second;
-			else {
+			if (p != createdLabels.end())
+				counter = p->second->value;
+			else if (p == createdLabels.end()) {
 				cout << "label not found\n";
 				exit(1);
 			}
 		}
-		else
-			counter = linenumber;
 	}
-	else if (y[0] == '$') {
-		if (q != labelclass.end() && q->second != 0 && p != labelclass.end())
-			counter = p->second;
-		else if (q != labelclass.end() && q->second == 0 && p != labelclass.end())
+	else if (y[0] = '$') {
+		if (q != createdVariable.end() && q->second->value != 0 && p != createdLabels.end())
+			counter = p->second->value;
+		else if (q != createdVariable.end() && q->second->value == 0 && p != createdLabels.end())
 			counter = linenumber;
 		else {
-			cout << "label not found\n";
+			cout << "label or variable not found\n";
 			exit(1);
 		}
 	}
